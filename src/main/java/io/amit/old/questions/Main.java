6b7127450ca86bd4bd4aef0d35f51cffa22e609f
amit96pal <amit96pal.ap@gmail.com>
2026-08-12 12:10:24 +0530

    CompletableFuture with ExecutorService

--- src/main/java/io/amit/old/questions/Main.java
+++ src/main/java/io/amit/old/questions/Main.java
@@ -1,27 +1,47 @@
 package io.amit.old.questions;
 
-import java.util.function.BiFunction;
-import java.util.function.BinaryOperator;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
-    public static void main(String[] args) {
-        String str = "fourhead";
-        printLuckyString(str);
+    private static final AtomicInteger THREAD_COUNTER = new AtomicInteger(1);
+
+    private static final ExecutorService executorService =
+            Executors.newFixedThreadPool(3, new ThreadFactory() {
+                @Override
+                public Thread newThread(Runnable runnable) {
+                    Thread thread = new Thread(runnable);
+                    thread.setName("my-custom-thread-" + THREAD_COUNTER.getAndIncrement());
+                    return thread;
+                }
+            });
+
+    public static void main(String[] args) throws InterruptedException {
+        List<String> names = List.of("amit", "karan", "aman", "vikas");
+
+        List<CompletableFuture<Void>> futures = names.stream()
+                .map(Main::printName)
+                .toList();
+
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
+
+        /**
+         * What does shutdown() do?
+         * It tells the executor:
+         * “Do not accept any new tasks, but complete already submitted tasks.”
+         */
+//        executorService.shutdown();
     }
 
-    private static void printLuckyString(String str) {
-        if(str.length() <= 1){
-            return;
-        }
-        int length = str.length();
-        int mid = (length+0)/2;
-
-        String left = str.substring(0, mid);
-        String right = str.substring(mid, length);
-
-        System.out.println("left "+left+" right "+right);
-
-        printLuckyString(left);
-        printLuckyString(right);
+    public static CompletableFuture<Void> printName(String name) {
+        return CompletableFuture
+                .runAsync(() -> System.out.println("Thread: "+Thread.currentThread().getName()+" : "+name),
+                        executorService);
     }
+
+
 }
--- src/main/java/io/amit/old/questions/Q4.java
+++ src/main/java/io/amit/old/questions/Q4.java
@@ -1,17 +1,54 @@
 package io.amit.old.questions;
 
+import org.jspecify.annotations.NonNull;
+
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Q4 {
+    private static final AtomicInteger THREAD_COUNTER = new AtomicInteger(1);
+
+    private static final ExecutorService executorService =
+            Executors.newFixedThreadPool(3, new ThreadFactory() {
+                @Override
+                public Thread newThread(Runnable runnable) {
+                    Thread thread = new Thread(runnable);
+                    thread.setName("my-custom-thread-" + THREAD_COUNTER.getAndIncrement());
+                    return thread;
+                }
+            });
+
     public static void main(String[] args) throws InterruptedException {
-        List<String> names = List.of("amit", "aman", "vikas");
-        names.forEach(Q4::printName);
-        Thread.sleep(5000);
+        List<String> names = List.of("amit", "karan", "aman", "vikas");
+
+        List<CompletableFuture<Void>> futures = names.stream()
+                .map(Q4::printName)
+                .toList();
+
+        CompletableFuture
+                .allOf(futures.toArray(new CompletableFuture[0]))
+                .join();
+
+        /**
+         * What does shutdown() do?
+         * It tells the executor:
+         * “Do not accept any new tasks, but complete already submitted tasks.”
+         */
+        executorService.shutdown();
     }
 
-    public static void printName(String name) {
-        CompletableFuture
-                .runAsync(() -> System.out.println(Thread.currentThread().getName()+" : "+name));
+    public static CompletableFuture<Void> printName(String name) {
+        return CompletableFuture
+                .runAsync(printWithThreadDetais(name),
+                        executorService);
     }
+
+    private static @NonNull Runnable printWithThreadDetais(String name) {
+        return () -> System.out.println("Thread: " + Thread.currentThread().getName() + " : " + name);
+    }
+
 }
